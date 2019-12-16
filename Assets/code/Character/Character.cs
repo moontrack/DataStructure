0c9153944f8457e0505e5e311e055b5502efd0ac
moontrack <731590917@qq.com>
2019-12-16 21:41:28 +0800

    init

---
+++ Assets/code/Character/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+public class MainCharacter : MonoBehaviour
+{
+    public string Name;
+    public CharacterType Type;
+    //Status
+    private float mistakeRate;
+    public float MistakeRate { get => mistakeRate; set => mistakeRate = value; }
+    public virtual CharacterAction GetAction()
+    {
+        return CharacterAction.Null;
+    }
+}
+//
+public class Repeater : MainCharacter
+{
+    public override CharacterAction GetAction()
+    {
+        //补全
+        return CharacterAction.Fool;
+    }
+}
---
+++ Assets/code/Utility.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+#region Enum
+public enum CharacterType
+{
+    Repeater,
+}
+//
+public enum CharacterAction
+{
+    Fool,
+    Cooperate,
+    Null,
+}
+#endregion
+
+public class Utility
+{
+    #region Path
+    public static readonly string PATH_IMG = "Img/";
+    public static readonly string PATH_PREFAB = "Prefab/";
+    #endregion
+    //
+    #region Function
+    public static Sprite LoadSprite(string path)
+    {
+        try
+        {
+            return Resources.Load<Sprite>(path);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(path);
+            Debug.LogException(ex);
+            return null;
+        }
+    }
+    //
+    public static List<string> LoadScript(string path)
+    {
+        try
+        {
+            TextAsset textAsset = LoadTextAsset(path);
+            return Regex.Split(textAsset.text, "\r\n").ToList();
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(path);
+            Debug.LogException(ex);
+            return null;
+        }
+    }
+    //
+    public static TextAsset LoadTextAsset(string path)
+    {
+        try
+        {
+            return Resources.Load<TextAsset>(path);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(path);
+            Debug.LogException(ex);
+            return null;
+        }
+    }
+    #endregion
+}
