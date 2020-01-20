502ac778281ab2e34e2f4cbf7dd156c342db56de
LLLQAQ <229357794@qq.com>
2020-01-20 23:37:40 +0800

    更新教程框架，以及界面切换

--- Assets/code/StartMenu.cs
+++ Assets/code/StartMenu.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 
 public class StartMenu : MonoBehaviour
 {
     private AudioSource audiosource;
 
+    private GameObject o0, o1, o2, o3, o4, o5, o6, o7, tool;
+    private List<GameObject> Scene = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
+        //场景渲染初始化
+        o0 = GameObject.Find("StartScene");
+        Scene.Add(o0);
+        o1 = GameObject.Find("1SinglePlay");
+        Scene.Add(o1);
+        o2 = GameObject.Find("2MultiplePlay");
+        Scene.Add(o2);
+        o3 = GameObject.Find("3SingleGame");
+        Scene.Add(o3);
+        o4 = GameObject.Find("4MutipleGame");
+        Scene.Add(o4);
+        o5 = GameObject.Find("5Evolution");
+        Scene.Add(o5);
+        o6 = GameObject.Find("6Fault");
+        Scene.Add(o6);
+        //注意，第8场景的序号为7，因为7是沙盒模式
+        o7 = GameObject.Find("8Conclusion");
+        Scene.Add(o7);
+        tool = GameObject.Find("ToolBar");
+        tool.SetActive(true);
+        OnRender(0);
+
         //声音区块
         audiosource = gameObject.AddComponent<AudioSource>();
         AudioClip clip = Resources.Load<AudioClip>("images/sounds/bg_music");
@@ -22,7 +48,11 @@ public class StartMenu : MonoBehaviour
         List<string> btnsName = new List<string>();
         btnsName.Add("MusicButton");
         btnsName.Add("StartButton");
-        foreach(string _ in btnsName)
+        for (int i = 1; i < 9; i++)
+        {
+            btnsName.Add("Button" + i);
+        }
+        foreach (string _ in btnsName)
         {
             GameObject btnobject = GameObject.Find(_);
             Button btn = btnobject.GetComponent<Button>();
@@ -32,6 +62,7 @@ public class StartMenu : MonoBehaviour
             });
 
         }
+
     }
 
     // Update is called once per frame
@@ -47,6 +78,38 @@ public class StartMenu : MonoBehaviour
     {
         switch(sender.name)
         {
+            case "Button1":
+                Debug.Log("Button1");
+                OnRender(1);
+                break;
+            case "Button2":
+                Debug.Log("Button2");
+                OnRender(2);
+                break;
+            case "Button3":
+                Debug.Log("Button3");
+                OnRender(3);
+                break;
+            case "Button4":
+                Debug.Log("Button4");
+                OnRender(4);
+                break;
+            case "Button5":
+                Debug.Log("Button5");
+                OnRender(5);
+                break;
+            case "Button6":
+                Debug.Log("Button6");
+                OnRender(6);
+                break;
+            case "Button7":
+                Debug.Log("Button7");
+                SceneManager.LoadScene("MainScene");
+                break;
+            case "Button8":
+                Debug.Log("Button8");
+                OnRender(7);
+                break;
             case "MusicButton":
                 {
                     Debug.Log("MusicButton");
@@ -66,6 +129,14 @@ public class StartMenu : MonoBehaviour
             default:
                 Debug.Log("none");
                 break;
+        }
+    }
+    public void OnRender(int index)
+    {
+        for(int i=0;i<Scene.Count;i++)
+        {
+            if (i == index) Scene[i].SetActive(true);
+            else Scene[i].SetActive(false);
         }
     }
 }
